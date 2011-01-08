446ce313691b21d708a12904cc2136b3d5969fc8
lefebvreariane@gmail.com <lefebvreariane@gmail.com@2187a008-b734-0937-d5d4-3b54df4e424a>
2011-01-08 16:17:34 +0000

    ajout de la fonction RGB_to_grey(il faut cliquer sur l'image pour que ca marche, je sais pas trop pourquoi)

--- controleur.cpp
+++ controleur.cpp
@@ -7,7 +7,6 @@ Controleur::Controleur(ZoneDessin *zone)
     z = zone;
 }
 
-
 void Controleur::clic_recu()
 {
 
@@ -17,6 +16,10 @@ void Controleur::clic_recu()
         }
     case PIPETTE: {
             pipette(z->resultLabel->X0,z->resultLabel->Y0);
+        }
+    case GREY: {
+            RGB_to_grey();
+            break;
         }
     default: {
 
@@ -36,4 +39,25 @@ void Controleur::pipette(int x, int y)
 */
     emit afficher_pixel(pixel.red(), pixel.green(),pixel.blue());
 
+}
+
+void Controleur::RGB_to_grey()
+{   //lit l'image qui correspond au LabelImage (z->resultLabel)
+    //traite l'image
+    //affiche l'image correspondante
+
+    if(!z->image.isNull() && !z->image.allGray()){
+        double greyD;
+        int greyI,i,j;
+        for (i=0 ; i<z->image.width() ; i++){
+            for(j=0 ; j<z->image.height() ; j++){
+                QColor c = z->image.pixel(i,j);
+                greyD = c.red()*0.299 + c.green()*0.587 + c.blue()*0.114 + 0.5;
+                greyI = (int) greyD;
+                z->image.setPixel(i,j,qRgb(greyI,greyI,greyI));
+            }
+        }
+        //z->image.setColorCount(256);
+    }
+    z->afficher_image();
 }
